0917bedcde98d332fcc3a6c7818583bff7bdde9a
giovannaxavierm <giovanna-xavierm@hotmail.com>
2020-08-30 16:53:16 -0300

    first commit

---
+++ DDL.sql
@@ -0,0 +1,30 @@
+create schema springdb;
+
+use springdb;
+
+create user 'user'@'localhost' identified by 'alohomora';
+
+grant select, insert, delete, update on springdb.* to user@'localhost';
+
+create table usr_usuario (
+	usr_id bigint unsigned not null auto_increment,
+	usr_nome varchar(20) not null,
+	user_senha varchar(50) not null,
+	primary key (usr_id),
+	unique key uni_usuario_nome (usr_nome)
+);
+
+create table aut_autorizacao (
+	aut_id bigint unsigned not null auto_increment,
+	aut_nome varchar(20) not null,
+	primary key (aut_id),
+	unique key uni_aut_nome (aut_nome)
+);
+
+create table uau_usuario_autorizacao (
+	usr_id bigint unsigned not null auto_increment,
+	aut_id bigint unsigned not null auto_increment,
+	primary key (usr_id, aut_id),
+	foreign key aut_usuario_fk (usr_id) references usr_usuario (usr_id),
+	foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id)
+);
