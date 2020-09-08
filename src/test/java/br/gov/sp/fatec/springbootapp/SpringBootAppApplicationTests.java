df76de1d7469f8a3be063c3f1af308837e5b5e0d
giovannaxavierm <giovanna-xavierm@hotmail.com>
2020-09-08 01:13:55 +0000

    jpa3

--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -3,13 +3,18 @@ package br.gov.sp.fatec.springbootapp;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-
+import br.gov.sp.fatec.springbootapp.repository.AutorizacaoRepository;
 import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
+import br.gov.sp.fatec.springbootapp.entity.Autorizacao;
 import br.gov.sp.fatec.springbootapp.entity.Usuario;
 
 @SpringBootTest
@@ -20,6 +25,9 @@ class SpringBootAppApplicationTests {
     @Autowired
     private UsuarioRepository usuarioRepo;
 
+    @Autowired
+    private AutorizacaoRepository autRepo;
+
 	@Test
 	void contextLoads() {
 	}
@@ -29,8 +37,64 @@ class SpringBootAppApplicationTests {
         Usuario usuario = new Usuario();
         usuario.setNome("Jack");
         usuario.setSenha("pearson");
+        usuario.setAutorizacoes(new HashSet<Autorizacao>());
+        Autorizacao aut = new Autorizacao(); 
+        aut.setNome("ROLE_USUARIO");
+        autRepo.save(aut);
+        usuario.getAutorizacoes().add(aut);
         usuarioRepo.save(usuario);
-        assertNotNull(usuario.getId());
+        assertNotNull(usuario.getAutorizacoes().iterator().next().getId());
+    }
+
+    @Test
+    void testaInsercaoAutorizacao(){
+        Usuario usuario = new Usuario();
+        usuario.setNome("Jack");
+        usuario.setSenha("pearson");
+        usuarioRepo.save(usuario);
+        Autorizacao aut = new Autorizacao(); 
+        aut.setNome("ROLE_USUARIO");
+        aut.setUsuarios(new HashSet<Usuario>());
+        aut.getUsuarios().add(usuario);
+        autRepo.save(aut);
+        assertNotNull(aut.getUsuarios().iterator().next().getId());
+    } 
+
+    @Test
+    void testaAutorizacao(){
+        Usuario usuario = usuarioRepo.findById(1L).get();
+        
+        assertEquals("ROLE_ADMIN", usuario.getAutorizacoes().iterator().next().getNome());
+    }
+
+    @Test
+    void testaUsuario(){
+        Autorizacao aut = autRepo.findById(1L).get();
+        assertEquals("Luna", aut.getUsuarios().iterator().next().getNome());
+    }
+
+    @Test
+        void testaBuscaUsuarioNomeContains(){
+        List<Usuario> usuarios = usuarioRepo.findByNomeContainsIgnoreCase("L");
+        assertFalse(usuarios.isEmpty());
+    }
+
+     @Test
+        void testaBuscaUsuario(){
+        Usuario usuario = usuarioRepo.findByNome("Luna");
+        assertNotNull(usuario);
+    }
+
+    @Test
+        void testaBuscaUsuarioNomeSenha(){
+        Usuario usuario = usuarioRepo.findByNomeAndSenha("Luna","lovegood");
+        assertNotNull(usuario);
+    }
+
+    @Test
+        void testaBuscaUsuarioNomeAutorizacao(){
+        List<Usuario> usuarios = usuarioRepo.findByAutorizacoesNome("ROLE_ADMIN");
+        assertFalse(usuarios.isEmpty());
     }
 
 }
