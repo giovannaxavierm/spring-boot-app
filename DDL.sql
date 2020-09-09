94cf51bda9082e199be990b25c6f130e9318a1dd
giovannaxavierm <giovanna-xavierm@hotmail.com>
2020-09-09 23:54:45 +0000

    Alterações no sql

--- DDL.sql
+++ DDL.sql
@@ -29,8 +29,39 @@ create table uau_usuario_autorizacao (
   foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
 );
 
+create table liv_livro (
+  liv_id bigint unsigned not null auto_increment,
+  liv_nome varchar(50) not null,
+  liv_autor varchar(20) not null,
+  primary key (liv_id)
+);
+
+create table com_comentario (
+  com_id bigint unsigned not null auto_increment,
+  com_comentario varchar(100) not null,
+  liv_id bigint unsigned not null,
+  primary key (com_id),
+  foreign key cml_livro_fk (liv_id) references liv_livro (liv_id) on delete restrict on update cascade
+);
+
+
+
+create table cmu_comentario_usuario (
+  usr_id bigint unsigned not null,
+  com_id bigint unsigned not null,
+  primary key (usr_id, com_id),
+  foreign key cmu_usuario_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade,
+  foreign key cmu_comentario_fk (com_id) references com_comentario (com_id) on delete restrict on update cascade
+);
+
 insert into usr_usuario (usr_nome, usr_senha)
     values('Luna', 'Senha');
 insert into aut_autorizacao (aut_nome)
     values('ROLE_ADMIN');
 insert into uau_usuario_autorizacao (1, 1);
+insert into liv_livro (liv_nome, liv_autor)
+    values ('Harry Potter e o Prisioneiro de Azkaban', 'J.K Rowling');
+insert into com_comentario(com_comentario,liv_id)
+    values ('Emocionante. Melhor livro da saga.','1');
+insert into cmu_comentario_usuario(usr_id,com_id)
+    values ('1','1');
